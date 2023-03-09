99fd0d1081f53377106684690b6de882c6d63188
Camilo Castro <camos8710@gmail.com>
2023-03-08 21:02:49 -0500

    Validate form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,80 @@
+import { Fragment, useState } from "react";
+import Card from "./Card";
+
 function App() {
   //Aqui deberias agregar los estados y los handlers para los inputs
+  const [nombre, setNombre] = useState('');
+  const [cancion, setCancion] = useState('');
+
+  const [isFormValid, setFormValidation] = useState(false);
+
+
+  const handlerChangeName = (e) => {
+    console.log('evento nombre', e.target.value);
+    let name = e.target.value;
+    if (name.length <= 3) {
+      setFormValidation(true);
+      console.log('El nombre tienes menos de 3 caracteres..!', isFormValid);
+    }
+    setNombre(name.trim());
+    
+  };
+
+  const handlerChangeCancion = (e) => {
+    console.log('evento cancion', e.target.value);
+    let cancion = e.target.value;
+
+    if (cancion.length > 6) {
+      setFormValidation(true);
+      console.log('La cancion tiene mas de 6 caracteres..!', isFormValid);
+    }
+    setCancion(e.target.value);
+  };
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    console.log('evento form submit', e.target);
+    const dataForm = {
+      nombre,
+      cancion
+    }
+
+    console.log('Evento boton', dataForm);
+  };
 
   return (
-    <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
-    </div>
+    <Fragment>
+      <div className="App">
+        <h1>¿Que musica te gusta?</h1>
+        <form onSubmit={handlerSubmit}>
+          {/* aqui deberias escribir tu codigo */}
+
+          <input 
+            placeholder="nombre" 
+            id="nombre"
+            type="text"
+            name="nombre"
+            value={nombre}
+            onChange={handlerChangeName}
+            minLength={3}
+          />
+          <input 
+            placeholder="canción"
+            id="cancion"
+            type="text"
+            name="cancion"
+            value={cancion}
+            onChange={handlerChangeCancion}
+
+          />
+
+          <button type="submit" disabled={isFormValid}>Enviar</button>
+
+
+        </form>
+      </div>
+      <Card nombre={nombre} cancion={cancion}/>
+    </Fragment>
   );
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,15 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card(props = {
+  nombre,
+  cancion
+}) {
   return (
     <div>
       <h2>Esto es un componente</h2>
+      <span>{props.nombre}</span>
+      <span>{props.cancion}</span>
     </div>
   );
 }
